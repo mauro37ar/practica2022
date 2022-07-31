c6ae67ee0e7f472ed385298078cf234569cd4eb4
mauro40ar <mauro39ar@gmail.com>
2022-07-30 21:28:09 -0300

    formulario

---
+++ pagina/Pagapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PagappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Pagapp'
---
+++ pagina/Pagapp/form.py
@@ -0,0 +1,11 @@
+from argparse import Action
+from ast import For
+from cProfile import label
+from django.forms import Form, Input
+
+
+<form action="/su nombre/"  method="POST">
+    <label for="/su nombre/"> su nombre :</label>
+    <input id="su nombre" type="text" name="su nombre" value="{{current_name}}">
+    <input type="submit" value="OK">
+</form>
